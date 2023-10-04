3b2081653e59aeb35180fa6d1a12a5667334066c
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2023-10-04 20:10:21 +0000

    Versione Funzionante

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <a href="/dispensa/">Visualizza dispensa</a>
+        <br>
+        <a href="/esempio-html/">Visualizza Esempio HTML</a>
+    </body>
+</html>
