0d93601e5931cf1421e0dbcf61e5dad826ee42f6
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2023-10-07 09:52:24 +0200

    Praparazione per lezione

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
         <a href="/dispensa/">Visualizza dispensa</a>
         <br>
         <a href="/esempio-html/">Visualizza Esempio HTML</a>
+        <br>
+        <a href="/curriculim"> curriculim </a>
     </body>
 </html>
